4d7bc895bd304da01cfe60cfdaa021a4487e03ed
lProxyz <48080532+lProxyz@users.noreply.github.com>
2023-01-05 22:22:00 -0600

    Agregando formulario de contacto y carrusel

--- src/Components/Principal/Carrusel.jsx
+++ src/Components/Principal/Carrusel.jsx
@@ -50,102 +50,152 @@ const Carrusel = (props) => {
           }}
           navigation={true}
           modules={[Pagination, Navigation]}
-          className="p"
         >
-          <SwiperSlide
-            style={{ alignItems: "start"}}
-          >
-            <div className="border-2 border-gray-500 bg-white hover:bg-[#006DFF] hover:text-white text-[#10069F]">
-              <a className="bg-white" href="#">
-                <div className="h-[60px]">
+          <SwiperSlide style={{ alignItems: "start" }}>
+            <div className="link">
+              <a className="z-10 " href="#">
+                <div className="h-32 ">
                   <img
-                    className="object-top"
+                    className="object-center"
                     src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/Stories/2022/rr-ultrafan-content-image.jpg?h=300&iar=0&w=480"
                     alt=""
                   />
                 </div>
+
+                {/* //! Intentando coloar el estilo del boton */}
+
                 <div>
-                  <p className="ml-10 mr-32  text-[24px] text-start border-b mt-3 border-gray-300 tracking-wide ">
-                    Rolls-Royce UltraFan technology demonstrator build...
-                  </p>
-                </div>
-                <div className="grid grid-cols-2 ml-5">
-                  <div className="flex">
-                    <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
-                      press releases
-                    </p>
-                  </div>
                   <div>
-                    <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
-                      19 December 2022
+                    <p className=" text-start p-3 border-b border-gray-300">
+                      Rolls-Royce and Gulfstream give wings to...
                     </p>
+                    <div className="grid grid-cols-2 ml-2 justify-between">
+                      <div>
+                        <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
+                          press releases
+                        </p>
+                      </div>
+                      <div>
+                        <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
+                          16 December 2022
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </a>
             </div>
-          </SwiperSlide>
 
-          <SwiperSlide>
-            <div>
-              <a className="bg-white" href="#">
-                <div className="h-20 ">
+          </SwiperSlide>
+          <SwiperSlide style={{ alignItems: "start" }}>
+            <div className="link">
+              <a className="z-10 " href="#">
+                <div className="h-32 ">
                   <img
-                    className="object-top"
+                    className="object-center"
                     src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/Stories/2022/rr-gulfstream-content-image.jpg?h=300&iar=0&w=480"
                     alt=""
                   />
                 </div>
 
-                <p className="text-[#10069F] text-[22px] text-center mb-2 border-b border-gray-300">
-                  Rolls-Royce UltraFan technology demonstrater build...
-                </p>
-                <div className="grid grid-cols-2 ml-5">
+                {/* //! Intentando coloar el estilo del boton */}
+
+                <div>
                   <div>
-                    <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
-                      press releases
+                    <p className=" text-start p-3 border-b border-gray-300">
+                      Rolls-Royce UltraFan technology demonstrater build...
                     </p>
-                  </div>
-                  <div>
-                    <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
-                      19 December 2022
-                    </p>
+                    <div className="grid grid-cols-2 ml-2 justify-between">
+                      <div>
+                        <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
+                          press releases
+                        </p>
+                      </div>
+                      <div>
+                        <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
+                          19 December 2022
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </a>
             </div>
+
           </SwiperSlide>
-          <SwiperSlide>
-            <div className="pl-2">
-              <a className="bg-white" href="#">
-                <div className="h-20 ">
+
+          <SwiperSlide style={{ alignItems: "start" }}>
+            <div className="link">
+              <a className="z-10 " href="#">
+                <div className="h-32 ">
                   <img
-                    className=""
-                    src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/Stories/2022/rr-ultrafan-content-image.jpg?h=300&iar=0&w=480"
+                    className="object-center"
+                    src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/Stories/2022/story-a-5-minute-chat-with-marcin-dutka-01.jpg?h=200&iar=0&w=200"
                     alt=""
                   />
                 </div>
 
-                <p className="text-[#10069F] text-[22px] text-center mb-2 border-b border-gray-300">
-                  Rolls-Royce UltraFan technology demonstrater build...
-                </p>
-                <div className="grid grid-cols-2 ml-5">
+                {/* //! Intentando coloar el estilo del boton */}
+
+                <div>
                   <div>
-                    <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
-                      press releases
+                    <p className=" text-start p-3 pb-12 border-b border-gray-300">
+                      A 5-minute chat with Marcin Dutka
                     </p>
-                  </div>
-                  <div>
-                    <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
-                      19 December 2022
-                    </p>
+                    <div className="grid grid-cols-2 ml-2 justify-between">
+                      <div>
+                        <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
+                          our stories
+                        </p>
+                      </div>
+                      <div>
+                        <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
+                          13 December 2022
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </a>
             </div>
+
           </SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide style={{ alignItems: "start" }}>
+            <div className="link">
+              <a className="z-10 " href="#">
+                <div className="h-32 ">
+                  <img
+                    className="object-top transform hover:scale-100"
+                    src="https://www.rolls-royce.com/~/media/Images/R/Rolls-Royce/content-images/marine/discover/landing-images/pr-default-landing-image-v3.jpg?h=470&iar=0&w=500"
+                    alt=""
+                  />
+                </div>
+
+                {/* //! Intentando colocar el estilo del boton */}
+
+                <div>
+                  <div>
+                    <p className=" text-start p-3 pb-12 border-b border-gray-300">
+                      Rools-Royce, Pratt & Whitney, Virginia Tech...
+                    </p>
+                    <div className="grid grid-cols-2 ml-2 justify-between">
+                      <div>
+                        <p className="uppercase text-[#FA4692] font-bold text-[12px] py-3">
+                          press releases
+                        </p>
+                      </div>
+                      <div>
+                        <p className="uppercase text-[#CECED1] font-bold text-[12px] py-3">
+                          09 December 2022
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+          </SwiperSlide>
           <SwiperSlide>Slide 7</SwiperSlide>
           <SwiperSlide>Slide 8</SwiperSlide>
           <SwiperSlide>Slide 9</SwiperSlide>
